11c61a1fba2cdb23d4985e2ed1e9ab872ef71c9c
YukJiSoo <wltn3231@naver.com>
2020-01-30 12:42:09 +0900

    chore: create button component에서 create form 분리
    create button component에서 create form component를 따로 분리했습니다.

--- client/src/components/CreateButton/index.tsx
+++ client/src/components/CreateButton/index.tsx
@@ -4,6 +4,38 @@ import { reducer, initialState } from "./reducer";
 
 import "./style.scss";
 
+interface CreateFormProps {
+	itemName: string;
+	handleCreateItem: (event: React.FormEvent<HTMLFormElement>) => void;
+	handleWriteItemName: (event: React.ChangeEvent<HTMLInputElement>) => void;
+	handleCancelWriteItemName: () => void;
+}
+
+function CreateForm({
+	itemName,
+	handleCreateItem,
+	handleWriteItemName,
+	handleCancelWriteItemName
+}: CreateFormProps) {
+	return (
+		<form className="Create-form" onSubmit={handleCreateItem}>
+			<input
+				type="text"
+				className="Name-input"
+				name="item-name"
+				value={itemName}
+				onChange={handleWriteItemName}
+			/>
+			<button className="Create-button" type="submit">
+				Create
+			</button>
+			<button className="Cancel-button" onClick={handleCancelWriteItemName}>
+				Cancel
+			</button>
+		</form>
+	);
+}
+
 interface CreateButtonProps {
 	title: string;
 	className?: string;
@@ -36,21 +68,12 @@ function CreateButton({
 				{title}
 			</h3>
 			{toggle && (
-				<form className="Create-form" onSubmit={handleCreateItem}>
-					<input
-						type="text"
-						className="Name-input"
-						name="item-name"
-						value={itemName}
-						onChange={handleWriteItemName}
-					/>
-					<button className="Create-button" type="submit">
-						Create
-					</button>
-					<button className="Cancel-button" onClick={handleCancelWriteItemName}>
-						Cancel
-					</button>
-				</form>
+				<CreateForm
+					itemName={itemName}
+					handleCreateItem={handleCreateItem}
+					handleWriteItemName={handleWriteItemName}
+					handleCancelWriteItemName={handleCancelWriteItemName}
+				/>
 			)}
 		</article>
 	);
